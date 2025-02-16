2eb4719d8eaeed6f20c10ec1f5de9b3508fa2e7b
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-02-16 16:04:36 +0500

    Update blog page headers and display order

--- src/app/Read/page.tsx
+++ src/app/Read/page.tsx
@@ -22,7 +22,7 @@ export default async function Read() {
   return (
     <>
       <main className="container mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold text-center mb-6">Latest Blogs</h1>
+        <h1 className="text-3xl font-bold text-center mb-6">Blogs</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {data.reverse().map((post, index) => (
             <Card key={index} className="shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-200">
